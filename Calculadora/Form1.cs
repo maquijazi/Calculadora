2c0db3d08fe1bdce8661d2b7ec3c1a05b26c21b7
maquijazi <xavi90_8@hotmail.com>
2020-03-17 21:33:14 +0100

    Agregar archivos de proyecto.

---
+++ Calculadora/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        /*Aqui declaro las variables de instancia*/
+
+        //operando1 guardará el primer número
+        double operando1 = 0;
+        //en el string de la operación guardo la operación
+        //que ha sido pulsada
+        String operacion = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            if (label1.Text == "0")
+            {
+                label1.Text = boton.Text;
+            }
+            else {
+                label1.Text = label1.Text + boton.Text;
+            }
+            
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            operacion = boton.Text;
+            operando1 = Convert.ToDouble(label1.Text);
+            label1.Text = "0";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            double operando2 = Convert.ToDouble(label1.Text);
+            double resultado = 0;
+            if (operacion == "+")
+            {
+                resultado = operando1 + operando2;
+            }
+            if (operacion == "-")
+            {
+                resultado = operando1 - operando2;
+            }
+
+            label1.Text = Convert.ToString(resultado);
+        }
+    }
+}
